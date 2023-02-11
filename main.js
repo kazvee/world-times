8fb4da7dd58705e871ea9c159dff62e43b27fc34
kazvee <kayvee123@gmail.com>
2023-02-11 16:21:13 -0700

    Rearrange format & html tags

--- main.js
+++ main.js
@@ -1,59 +1,65 @@
 import '@picocss/pico';
 import './style.css';
 import './clock.js';
-
 document.querySelector('#app').innerHTML = `
-<main class="container">
-<nav>
-  <ul>
-    <li><strong>World Clock</strong></li>
-  </ul>
- </nav>
-
-<div class="grid">
-  <div>
-    <article>
-    <header>Calgary</header>
-      <div id="Calgary"></div>
-    </article>
-  </div>
-
-  <div>
-    <article>
-    <header>New York</header>
-      <div id="New York"></div>
-    </article>
-  </div>
-
-  <div>
-    <article>
-    <header>Reykjavík</header>
-      <div id="Reykjavík"></div>
-    </article>
-  </div>
-
-  <div>
-    <article>
-    <header>Dublin</header>
-      <div id="Dublin"></div>
-    </article>
-  </div>
-
-  <div>
-    <article>
-    <header>Berlin</header>
-      <div id="Berlin"></div>
-    </article>
-  </div>
-
-  <div>
-    <article>
-    <header>Tel-Aviv</header>
-      <div id="Tel-Aviv"></div>
-    </article>
-  </div>
-
-  </div>
-
-  </main>
+<body>
+    <main class="container">
+        <aside>
+        <nav>
+            <ul>
+                <hgroup>
+                    <h1>World Clock</h1>
+                    <h2>
+                        <ins>
+                        Available in selected cities
+                        </ins>
+                    </h2>
+                </hgroup>
+            </ul>
+        </nav>
+        <div class="grid">
+        <div>
+            <article>
+                <header>
+                    Calgary
+                </header>
+                <div id="Calgary">
+                </div>
+            </article>
+        </div>
+        <div>
+            <article>
+                <header>New York</header>
+                <div id="New York"></div>
+            </article>
+        </div>
+        <div>
+            <article>
+                <header>
+                    Reykjavík
+                </header>
+                <div id="Reykjavík">
+                </div>
+            </article>
+        </div>
+        <div>
+            <article>
+                <header>Dublin</header>
+                <div id="Dublin"></div>
+            </article>
+        </div>
+        <div>
+            <article>
+                <header>Berlin</header>
+                <div id="Berlin"></div>
+            </article>
+        </div>
+        <div>
+            <article>
+                <header>Tel-Aviv</header>
+                <div id="Tel-Aviv"></div>
+            </article>
+        </div>
+</body>
+</main>
 `;
